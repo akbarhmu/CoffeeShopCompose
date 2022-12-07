657e1826f182b366aa5f3f53807dff224186fe93
Akbar Hamaminatu <akbar@ngoding.id>
2022-12-07 10:12:10 +0700

    feat: Add category item ui component

---
+++ app/src/main/java/id/ngoding/composecoffee/ui/components/CategoryItem.kt
@@ -0,0 +1,61 @@
+package id.ngoding.composecoffee.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import id.ngoding.composecoffee.R
+import id.ngoding.composecoffee.model.Category
+import id.ngoding.composecoffee.ui.theme.ComposeCoffeeTheme
+
+@Composable
+fun CategoryItem(
+    category: Category,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = category.imageCategory),
+            contentDescription = stringResource(
+                id = category.textCategory
+            ),
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(id = category.textCategory),
+            fontSize = 10.sp,
+            modifier = Modifier.paddingFromBaseline(top = 16.dp, bottom = 8.dp)
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryItemPreview() {
+    ComposeCoffeeTheme {
+        CategoryItem(
+            category = Category(
+                R.drawable.icon_category_cappuccino,
+                R.string.category_cappuccino
+            ),
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+    }
+}
