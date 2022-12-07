b7673809d44eb3b960c9d784c4fdfc26c7a5b8cd
Akbar Hamaminatu <akbar@ngoding.id>
2022-12-07 09:55:51 +0700

    feat: Add banner section to main activity

--- app/src/main/java/id/ngoding/composecoffee/MainActivity.kt
+++ app/src/main/java/id/ngoding/composecoffee/MainActivity.kt
@@ -3,14 +3,22 @@ package id.ngoding.composecoffee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.ngoding.composecoffee.ui.components.SearchBar
 import id.ngoding.composecoffee.ui.theme.ComposeCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +34,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeCoffeeApp() {
+    Column {
+        Banner()
+    }
+}
 
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
 }
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
