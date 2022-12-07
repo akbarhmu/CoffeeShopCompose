0f122759616785dcbf1c133c8a6b5595ef952430
Akbar Hamaminatu <akbar@ngoding.id>
2022-12-07 09:36:02 +0700

    Setup Fresh Android Jetpack Compose Project

---
+++ app/src/main/java/id/ngoding/composecoffee/MainActivity.kt
@@ -0,0 +1,43 @@
+package id.ngoding.composecoffee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import id.ngoding.composecoffee.ui.theme.ComposeCoffeeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCoffeeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeCoffeeTheme {
+        Greeting("Android")
+    }
+}
