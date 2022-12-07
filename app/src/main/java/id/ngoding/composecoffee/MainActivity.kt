6606848c212f9a8992b3d1f9b02124d9c9b6faf9
Akbar Hamaminatu <akbar@ngoding.id>
2022-12-07 10:53:22 +0700

    feat: Add favorite menu and best seller menu section

--- app/src/main/java/id/ngoding/composecoffee/MainActivity.kt
+++ app/src/main/java/id/ngoding/composecoffee/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -18,8 +20,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import id.ngoding.composecoffee.model.Menu
+import id.ngoding.composecoffee.model.dummyBestSellerMenu
 import id.ngoding.composecoffee.model.dummyCategory
+import id.ngoding.composecoffee.model.dummyMenu
 import id.ngoding.composecoffee.ui.components.CategoryItem
+import id.ngoding.composecoffee.ui.components.MenuItem
 import id.ngoding.composecoffee.ui.components.SearchBar
 import id.ngoding.composecoffee.ui.components.SectionText
 import id.ngoding.composecoffee.ui.theme.ComposeCoffeeTheme
@@ -37,10 +43,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeCoffeeApp() {
-    Column {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Banner()
         SectionText(title = stringResource(id = R.string.section_category))
         CategoryRow()
+        SectionText(title = stringResource(id = R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(title = stringResource(id = R.string.section_best_seller_menu))
+        MenuRow(listMenu = dummyBestSellerMenu)
     }
 }
 
@@ -74,6 +84,22 @@ fun CategoryRow(
     }
 }
 
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu = menu)
+        }
+    }
+}
+
 @Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
 fun ComposeCoffeeAppPreview() {
@@ -87,5 +113,13 @@ fun ComposeCoffeeAppPreview() {
 fun CategoryRowPreview() {
     ComposeCoffeeTheme {
         CategoryRow()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MenuRowPreview() {
+    ComposeCoffeeTheme {
+        MenuRow(dummyMenu)
     }
 }
